d6bbd4d5758bfa211a4e9c5cdd7825ca6fb2cae0
Hager Atef <hageratefabdelfattah@gmail.com>
2025-09-26 11:31:15 +0300

    Add project files.

---
+++ C44-G01-MVC04.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace C44_G01_MVC04.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
