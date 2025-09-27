b7ceb8d420eb7272e6d555389f2028319a876c14
Hager Atef <hageratefabdelfattah@gmail.com>
2025-09-27 12:42:05 +0300

    Video 02 & 03 (Index &create)

--- C44-G01-MVC04.BLL/Dto's/DepartmentDto's/CreatedDepartmentDto.cs
+++ C44-G01-MVC04.BLL/Dto's/DepartmentDto's/CreatedDepartmentDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,9 @@ namespace C44_G01_MVC04.BLL.Dto_s.DepartmentDto_s
 {
     public class CreatedDepartmentDto
     {
+        [Required(ErrorMessage ="Name is Required")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Code is Required")]
         public string Code { get; set; }
         public string? Description { get; set; }
     }
--- C44-G01-MVC04.PL/Controllers/DepartmentController.cs
+++ C44-G01-MVC04.PL/Controllers/DepartmentController.cs
@@ -1,4 +1,5 @@
-﻿using C44_G01_MVC04.BLL.Services;
+﻿using C44_G01_MVC04.BLL.Dto_s.DepartmentDto_s;
+using C44_G01_MVC04.BLL.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace C44_G01_MVC04.PL.Controllers
@@ -6,16 +7,56 @@ namespace C44_G01_MVC04.PL.Controllers
     public class DepartmentController : Controller
     {
         private readonly IDepartmentServices departmentServices;
+        private readonly ILogger<DepartmentController> logger;
+        private readonly IWebHostEnvironment webHost;
 
-        public DepartmentController(IDepartmentServices department)
+        public DepartmentController(IDepartmentServices department,ILogger<DepartmentController> logger, IWebHostEnvironment webHost)
         {
             this.departmentServices = department;
+            this.logger = logger;
+            this.webHost = webHost;
         }
 
         public IActionResult Index()
         {
             var dept = departmentServices.GetAllDepartments();
             return View(dept);
+        }
+
+        [HttpGet]
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(CreatedDepartmentDto dept)
+        {
+            try 
+            { 
+                if (ModelState.IsValid)
+                {
+                int result = departmentServices.AddDepartment(dept);
+
+                if (result > 0) return RedirectToAction("Index");
+                else ModelState.AddModelError(string.Empty, "Department Can't be created");
+                return View(dept);
+                }
+                else
+                {
+                    return View(dept);
+                }
+            } 
+            catch(Exception ex) 
+            {
+                if (webHost.IsDevelopment())
+                {
+                    logger.LogError(ex.Message);
+                    return View(dept);
+                }
+                else
+                {
+                    throw;
+
+                } 
+            }
         }
     }
 }
