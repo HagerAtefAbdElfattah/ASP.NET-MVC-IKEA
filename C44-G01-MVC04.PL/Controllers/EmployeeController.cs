c668f718a18942d9f09167ec104cd7fc5e658b7c
Hager Atef <hageratefabdelfattah@gmail.com>
2025-10-09 00:47:55 +0300

    video10-11

--- C44-G01-MVC04.PL/Controllers/EmployeeController.cs
+++ C44-G01-MVC04.PL/Controllers/EmployeeController.cs
@@ -1,6 +1,9 @@
-﻿using C44_G01_MVC04.BLL.Dto_s.EmployeeDto_s;
+﻿using C44_G01_MVC04.BLL.Dto_s.DepartmentDto_s;
+using C44_G01_MVC04.BLL.Dto_s.EmployeeDto_s;
 using C44_G01_MVC04.BLL.Services.DepartmentsServices;
 using C44_G01_MVC04.BLL.Services.EmployeesServices;
+using C44_G01_MVC04.PL.ViewModels.DepartmentVms;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Mvc;
 
 namespace C44_G01_MVC04.PL.Controllers
@@ -75,5 +78,112 @@ namespace C44_G01_MVC04.PL.Controllers
             return View(employee);
         }
 
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null) return BadRequest();
+
+            var employee = employeeServices.GetEmployeeById(id.Value);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            var viewEmployee = new UpdatedEmployeeDto()
+            {
+              Id = employee.Id,
+                Name = employee.Name,
+                Age = employee.Age,
+                Address = employee.Address,
+                Salary = employee.Salary,
+                IsActive = employee.IsActive,
+                Email = employee.Email,
+                PhoneNumber = employee.PhoneNumber,
+                HiringDate = employee.HiringDate,
+            };
+
+            return View(viewEmployee);
+        }
+
+        [ HttpPost]
+        public IActionResult Edit([FromRoute] int? id, UpdatedEmployeeDto model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            var emp = new UpdatedEmployeeDto()
+            {
+               Id = id.Value,
+                Name = model.Name,
+                Age = model.Age,
+                Address = model.Address,
+                Salary = model.Salary,
+                IsActive = model.IsActive,
+                Email = model.Email,
+                PhoneNumber = model.PhoneNumber,
+                HiringDate = model.HiringDate,
+            };
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    int result = employeeServices.UpdateEmployee(emp);
+
+                    if (result > 0) return RedirectToAction("Index");
+                    else ModelState.AddModelError(string.Empty, "Employee Can't be updated");
+                    return View(model);
+                }
+                else
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                if (environment.IsDevelopment())
+                {
+                    logger.LogError(ex.Message);
+                    return View(emp);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+        }
+
+        [HttpGet]
+        public IActionResult Delete([FromRoute] int? id)
+        {
+            if (id == null) return BadRequest();
+
+            var employee = employeeServices.GetEmployeeById(id.Value);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var result = employeeServices.DeleteEmployee(id);
+            if (result > 0) { return RedirectToAction("Index"); }
+            else
+            {
+                var employee = employeeServices.GetEmployeeById(id);
+
+                if (employee == null)
+                {
+                    return NotFound();
+                }
+                ModelState.AddModelError(string.Empty, "something went wrong!");
+                return View(employee);
+            }
+        }
     }
 }
