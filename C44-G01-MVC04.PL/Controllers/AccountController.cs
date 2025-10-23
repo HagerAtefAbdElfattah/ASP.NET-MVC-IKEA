be1b9ded35bf3fa0bbd7b8e577f146d24adf1147
Hager Atef <hageratefabdelfattah@gmail.com>
2025-10-23 17:06:24 +0300

    Session06-4.SignUp Logic & Implement Identity DI

--- C44-G01-MVC04.PL/Controllers/AccountController.cs
+++ C44-G01-MVC04.PL/Controllers/AccountController.cs
@@ -1,13 +1,59 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using C44_G01_MVC04.DAL.Models.Identity;
+using C44_G01_MVC04.PL.ViewModels.AccountVms;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol;
 
 namespace C44_G01_MVC04.PL.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
         [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel registerViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(registerViewModel);
+            }
+            var validUser = userManager.FindByNameAsync(registerViewModel.UserName).Result;
+            if (validUser != null)
+            {
+                ModelState.AddModelError("", "Username is already taken");
+                return View(registerViewModel);
+            }
+            var user = new ApplicationUser
+            {
+                UserName = registerViewModel.UserName,
+                Email = registerViewModel.Email,
+                FName = registerViewModel.FirstName,
+                LName = registerViewModel.LastName
+            };
+
+            var result = userManager.CreateAsync(user, registerViewModel.Password).Result;
+            if (result.Succeeded)
+            { 
+                return RedirectToAction(nameof(SignIn));
+            }
+            else {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(registerViewModel);
         }
 
         public IActionResult SignIn()
--- C44-G01-MVC04.PL/Program.cs
+++ C44-G01-MVC04.PL/Program.cs
@@ -3,9 +3,11 @@ using C44_G01_MVC04.BLL.Common.Services.Attachments;
 using C44_G01_MVC04.BLL.Services.DepartmentsServices;
 using C44_G01_MVC04.BLL.Services.EmployeesServices;
 using C44_G01_MVC04.DAL.Contexts;
+using C44_G01_MVC04.DAL.Models.Identity;
 using C44_G01_MVC04.DAL.Repositories.DepartmentRepo;
 using C44_G01_MVC04.DAL.Repositories.EmployeeRepo;
 using C44_G01_MVC04.DAL.UOW;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Build.Evaluation;
 using Microsoft.EntityFrameworkCore;
@@ -30,6 +32,17 @@ namespace C44_G01_MVC04.PL
                 option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
                 option.UseLazyLoadingProxies();
             });
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => 
+            {
+                //options.Password.RequireDigit = true;
+                //options.Password.RequiredLength = 8;
+                //options.Password.RequireNonAlphanumeric = false;
+                //options.Password.RequireUppercase = true;
+                //options.Password.RequireLowercase = true;
+                //options.User.RequireUniqueEmail = true;
+                //options.SignIn.RequireConfirmedEmail = false;
+            }).AddEntityFrameworkStores<ApplicationDbContext>();
+
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
             //builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
             builder.Services.AddScoped<IDepartmentServices, DepartmentServices>();
