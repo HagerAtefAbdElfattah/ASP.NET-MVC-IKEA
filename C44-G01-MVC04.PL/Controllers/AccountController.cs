87ed817d3f95c9e09cbd1b450b37741f042cb7c0
Hager Atef <hageratefabdelfattah@gmail.com>
2025-10-23 16:00:55 +0300

    Session08-video 3.Module Template

---
+++ C44-G01-MVC04.PL/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace C44_G01_MVC04.PL.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
